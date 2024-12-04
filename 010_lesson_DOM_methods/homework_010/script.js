aa992847403f305fc3bddd1bac82ed78f4c86f8b
german <german.kisin.box@gmail.com>
2024-12-04 21:13:09 +0100

    commit text

---
+++ 010_lesson_DOM_methods/homework_010/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("#magic-btn");
+document.body.appendChild(btn);
+btn.addEventListener("click", () => {
+  let newElement = document.createElement("button");
+  newElement.setAttribute("id", "magic-btn-2"); // Устанавливаем id
+  newElement.innerText = "🤬 Я изменю тебя!";
+  newElement.style.backgroundColor = "#a78b71";
+  newElement.style.color = "white";
+  document.body.append(newElement);
+
+  const btn2 = document.querySelector("#magic-btn-2");
+  btn2.addEventListener("click", () => {
+    btn.style.backgroundColor = "#9c4a1a";
+    btn.style.color = "black";
+  });
+});
