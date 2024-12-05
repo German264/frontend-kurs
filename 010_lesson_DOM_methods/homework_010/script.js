3b9012a8440e619d1b4ad161bc986a17d2fd12a0
german <german.kisin.box@gmail.com>
2024-12-05 15:52:04 +0100

    commit text

--- 010_lesson_DOM_methods/homework_010/script.js
+++ 010_lesson_DOM_methods/homework_010/script.js
@@ -1,15 +1,15 @@
 const btn = document.querySelector("#magic-btn");
-btn.addEventListener("click", () => {
-  let newElement = document.createElement("button");
-  newElement.setAttribute("id", "magic-btn-2"); // Устанавливаем id
-  newElement.innerText = "🤬 Я изменю тебя!";
-  newElement.style.backgroundColor = "#a78b71";
-  newElement.style.color = "white";
-  document.body.append(newElement);
 
-  const btn2 = document.querySelector("#magic-btn-2");
-  btn2.addEventListener("click", () => {
-    btn.style.backgroundColor = "#9c4a1a";
-    btn.style.color = "black";
-  });
+let btn2 = document.createElement("button");
+btn2.setAttribute("id", "magic-btn-2"); // Устанавливаем id
+btn2.innerText = "🤬 Я изменю тебя!";
+btn2.style.backgroundColor = "#a78b71";
+btn2.style.color = "white";
+
+btn.addEventListener("click", () => {
+  document.body.append(btn2);
+});
+btn2.addEventListener("click", () => {
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";
 });
