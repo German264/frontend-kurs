20b32933e2bcbacf3f7e4fe7ea979a7da1717860
german <german.kisin.box@gmail.com>
2024-12-06 10:29:26 +0100

    commit text

---
+++ 012_lesson_Arrays/script.js
@@ -0,0 +1,21 @@
+console.log("hello, arrays!")
+
+// ! arrays
+// сложный тип данных
+// синтаксис массива вкл. квадратные скобки
+
+let myNumbers = [1,2,3,4,5]
+let fruits = ["orange", "apple", "banana"]
+
+let item = fruits[0]
+
+let secret = fruits[10]
+
+let arr1 = [1, false, undefined, "strawberry"]
+console.log(arr1)
+
+let length = arr1.length
+console.log("длинна массива arr1: ", length)
+
+let lastElement = arr1[arr1.length - 1]
+console.log(lastElement)
